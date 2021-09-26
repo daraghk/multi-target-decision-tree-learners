0eacf03d287302e809aad4f19de2862e1de1c30f
Daragh <daragh.king.2014@gmail.com>
2021-09-26 14:36:49 +0100

    adding tests for question struct

--- src/utils/calculations.rs
+++ src/utils/calculations.rs
@@ -1,4 +1,7 @@
-use crate::{class_counter::{self, ClassCounter}, question::Question};
+use crate::{
+    class_counter::{self, ClassCounter},
+    question::Question,
+};
 use core::num;
 use std::hash::*;
 
@@ -36,14 +39,16 @@ pub fn gini(class_counts: &ClassCounter<i32, i32>, number_of_rows: f32) -> f32 {
     impurity - reduction
 }
 
-pub fn partition<'a>(data: &'a Vec<Vec<i32>>, question: &Question) -> (Vec<&'a Vec<i32>>, Vec<&'a Vec<i32>>) {
+pub fn partition<'a>(
+    data: &'a Vec<Vec<i32>>,
+    question: &Question,
+) -> (Vec<&'a Vec<i32>>, Vec<&'a Vec<i32>>) {
     let mut true_rows = vec![];
     let mut false_rows = vec![];
-    data.iter().for_each(|row|{
-        if(question.solve(row)){
+    data.iter().for_each(|row| {
+        if (question.solve(row)) {
             true_rows.push(row);
-        }
-        else{
+        } else {
             false_rows.push(row);
         }
     });
@@ -90,7 +95,7 @@ mod tests {
     }
 
     #[test]
-    fn test_partition_all_false(){
+    fn test_partition_all_false() {
         let data = vec![vec![1, 2, 1], vec![1, 2, 2], vec![1, 2, 3]];
         let question = Question::new(0, false, 2);
         let partitioned_data = partition(&data, &question);
@@ -100,7 +105,7 @@ mod tests {
     }
 
     #[test]
-    fn test_partition_all_true(){
+    fn test_partition_all_true() {
         let data = vec![vec![1, 2, 1], vec![1, 2, 2], vec![1, 2, 3]];
         let question = Question::new(0, false, 0);
         let partitioned_data = partition(&data, &question);
@@ -110,7 +115,7 @@ mod tests {
     }
 
     #[test]
-    fn test_partition_even(){
+    fn test_partition_even() {
         let data = vec![vec![1, 2, 1], vec![2, 2, 2], vec![4, 2, 3], vec![5, 2, 3]];
         let question = Question::new(0, false, 3);
         let partitioned_data = partition(&data, &question);
@@ -118,5 +123,4 @@ mod tests {
         assert_eq!(partitioned_data.0.len(), 2);
         assert_eq!(partitioned_data.1.len(), 2);
     }
-    
 }
--- src/utils/question.rs
+++ src/utils/question.rs
@@ -22,3 +22,36 @@ impl Question{
         val_to_check >= self.value
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_question_solve_numerical_true(){
+        let question = Question::new(0, false, 1);
+        let data_row = vec![1, 2, 4];
+        assert_eq!(question.solve(&data_row), true);
+    }
+
+    #[test]
+    fn test_question_solve_numerical_false(){
+        let question = Question::new(1, false, 5);
+        let data_row = vec![1, 2, 4];
+        assert_eq!(question.solve(&data_row), false);
+    }
+
+    #[test]
+    fn test_question_solve_categorical_true(){
+        let question = Question::new(0, true, 1);
+        let data_row = vec![1, 2, 4];
+        assert_eq!(question.solve(&data_row), true);
+    }
+
+    #[test]
+    fn test_question_solve_categorical_false(){
+        let question = Question::new(1, true, 5);
+        let data_row = vec![1, 2, 4];
+        assert_eq!(question.solve(&data_row), false);
+    }
+}
