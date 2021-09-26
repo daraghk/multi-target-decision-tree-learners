a5abdf7349e8609c36bbfa1bc7df51862b37eb65
Daragh <daragh.king.2014@gmail.com>
2021-09-26 14:58:12 +0100

    change to TreeNode

--- src/tree/node.rs
+++ src/tree/node.rs
@@ -1,7 +1,22 @@
 use crate::question::Question;
 
-pub struct Node<'a>{
+#[derive(Debug)]
+pub struct TreeNode<'a> {
     question: Question,
     true_branch: &'a Vec<Vec<i32>>,
     false_branch: &'a Vec<Vec<i32>>,
+}
+
+impl<'a> TreeNode<'a> {
+    pub fn new(
+        question: Question,
+        true_branch: &'a Vec<Vec<i32>>,
+        false_branch: &'a Vec<Vec<i32>>,
+    ) -> Self {
+        Self {
+            question,
+            true_branch,
+            false_branch,
+        }
+    }
 }
