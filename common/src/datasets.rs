90a340e27b6542d41ab78faa02f9e8fb27ee1e62
Daragh <daragh.king.2014@gmail.com>
2021-10-21 11:48:17 +0100

    adding classification tests for single target tree and experimenting with enums

--- common/src/datasets.rs
+++ common/src/datasets.rs
@@ -9,3 +9,28 @@ pub struct MultiTargetDataSet {
     pub features: Vec<Vec<f32>>,
     pub labels: Vec<Vec<f32>>,
 }
+
+#[derive(Debug)]
+pub enum Data {
+    SingleTarget(DataSet),
+    MultiTarget(MultiTargetDataSet),
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::data_reader::read_csv_data;
+
+    use super::Data;
+
+    #[test]
+    fn test_build_tree() {
+        let data_set = read_csv_data("./../common/data_files/iris.csv");
+        let single_target = Data::SingleTarget(data_set);
+        match single_target {
+            Data::SingleTarget(_) => {
+                println!("{:?}", single_target);
+            }
+            _ => {}
+        }
+    }
+}
--- decision-tree/src/utils/classifier.rs
+++ decision-tree/src/utils/classifier.rs
@@ -1,29 +1,44 @@
+use common::datasets::DataSet;
+
 use crate::{leaf::Leaf, node::TreeNode};
 
-fn classify(feature_row: &Vec<f32>, node: Box<TreeNode>) -> Leaf {
-    if !node.is_leaf_node() {
-        if node.question.solve(feature_row) {
-            return classify(feature_row, node.true_branch.unwrap());
-        } else {
-            return classify(feature_row, node.false_branch.unwrap());
+pub fn calculate_accuracy(test_data: &DataSet, tree_root: &Box<TreeNode>) -> f32 {
+    let mut accuracy = 0.;
+    for i in 0..test_data.features.len() {
+        let prediction = predict_class(&test_data.features[i], tree_root);
+        let actual = test_data.labels[i];
+        println!("Prediction: {}, Actual: {}", prediction, actual);
+        if prediction == actual {
+            accuracy += 1.;
         }
     }
-    node.leaf.unwrap()
+    accuracy / test_data.features.len() as f32
 }
 
-pub fn get_predicted_class(feature_row: &Vec<f32>, node: Box<TreeNode>) -> u32 {
+pub fn predict_class(feature_row: &Vec<f32>, node: &Box<TreeNode>) -> f32 {
     let leaf = classify(feature_row, node);
     let mut max = 0;
-    let mut max_class = 0;
-    let mut index = 0;
+    let mut max_class = 0.;
+    let mut index = 0.;
     leaf.predictions.counts.iter().for_each(|count| {
         if *count > max {
             max = *count;
             max_class = index;
         }
-        index += 1;
+        index += 1.;
     });
     max_class
+}
+
+fn classify<'a>(feature_row: &Vec<f32>, node: &'a Box<TreeNode>) -> &'a Leaf {
+    if !node.is_leaf_node() {
+        if node.question.solve(feature_row) {
+            return classify(feature_row, &node.true_branch.as_ref().unwrap());
+        } else {
+            return classify(feature_row, &node.false_branch.as_ref().unwrap());
+        }
+    }
+    node.leaf.as_ref().unwrap()
 }
 
 #[cfg(test)]
@@ -41,19 +56,45 @@ mod tests {
         let split_finder = SplitFinder::new(SplitMetric::Variance);
         let tree = DecisionTree::new(data_set, split_finder, 3);
         let row_to_classify = vec![58., 27., 51., 19.];
-        let predicted_class = get_predicted_class(&row_to_classify, Box::new(tree.root));
-        assert_eq!(predicted_class, 2);
+        let boxed_tree = Box::new(tree.root);
+        let predicted_class = predict_class(&row_to_classify, &boxed_tree);
+        assert_eq!(predicted_class, 2.);
     }
 
     use super::*;
     #[test]
-    fn test_classifier_unknown_data() {
+    fn test_print_classifier_result_unknown_data() {
         let data_set = read_csv_data("./../common/data_files/iris.csv");
         let split_finder = SplitFinder::new(SplitMetric::Variance);
         let tree = DecisionTree::new(data_set, split_finder, 3);
         let row_to_classify = vec![1., 23., 90., 10.];
-        let predicted_class = get_predicted_class(&row_to_classify, Box::new(tree.root));
+        let boxed_tree = Box::new(tree.root);
+        let predicted_class = predict_class(&row_to_classify, &boxed_tree);
         println!("{:?}", predicted_class);
+    }
+
+    #[test]
+    fn test_overall_accuracy_on_iris_training_data() {
+        let train_set = read_csv_data("./../common/data_files/iris.csv");
+        let split_finder = SplitFinder::new(SplitMetric::Variance);
+        let tree = DecisionTree::new(train_set, split_finder, 3);
+        let boxed_tree = Box::new(tree.root);
+
+        let test_set = read_csv_data("./../common/data_files/iris.csv");
+        let accuracy = calculate_accuracy(&test_set, &boxed_tree);
+        assert_eq!(accuracy, 1.0);
+    }
+
+    #[test]
+    fn test_overall_accuracy_on_iris_test_data() {
+        let train_set = read_csv_data("./../common/data_files/iris.csv");
+        let split_finder = SplitFinder::new(SplitMetric::Variance);
+        let tree = DecisionTree::new(train_set, split_finder, 3);
+        let boxed_tree = Box::new(tree.root);
+
+        let test_set = read_csv_data("./../common/data_files/iris_test.csv");
+        let accuracy = calculate_accuracy(&test_set, &boxed_tree);
+        assert_eq!(accuracy, 1.0);
     }
 
     #[test]
